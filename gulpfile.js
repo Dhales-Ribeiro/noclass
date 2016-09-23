96d7d6fbabbc25a9d32c209dabd2a211337bf7ae
Dhales Ribeiro <wm.dhales@gmail.com>
2016-09-23 13:06:38 -0300

    - criando tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,54 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var less = require('gulp-less');
+/*
+ |--------------------------------------------------------------------------
+ |	Core & Plugins
+ |--------------------------------------------------------------------------
+ */
+
+    var gulp        = require('gulp');
+    var concat      = require('gulp-concat');
+    var less        = require('gulp-less');
+    var clean       = require('gulp-clean');
 
 
-gulp.task('less', function () {
-    return console.log("teste");
-})
+
+/*
+ |--------------------------------------------------------------------------
+ |	Vars & Locations
+ |--------------------------------------------------------------------------
+ */
+
+    var biuldTeste      = './tests/';
+    var biuldDist       = './dist/';
+    var devDir          = './dev/';
+
+/*
+ |--------------------------------------------------------------------------
+ |	Clean Task
+ |--------------------------------------------------------------------------
+ */
+
+    gulp.task('cleanTest', function(){
+        return gulp.src([biuldTeste])
+            .pipe(clean());
+    });
+
+    gulp.task('cleanDist', function(){
+        return gulp.src([biuldDist])
+            .pipe(clean());
+    });
+
+
+/*
+ |--------------------------------------------------------------------------
+ |	Task teste pre-produção
+ |--------------------------------------------------------------------------
+ */
+
+    gulp.task('less', function () {
+        return gulp.src(devDir + 'LESS/_noClass.less')
+            .pipe(concat('noclass.css'))
+            .pipe(less())
+            .pipe(gulp.dest('./tests/css'));
+    });
+
+    gulp.task('default', ['less']);
